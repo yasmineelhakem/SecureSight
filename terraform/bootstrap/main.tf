851af5630ed31720e991d9852bff6014d17e6f79
yasmine <yasmine.elhakem@insat.ucar.tn>
2026-04-07 14:24:45 +0100

    provision s3 bucket for terraform state

---
+++ terraform/bootstrap/main.tf
@@ -0,0 +1,53 @@
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "s3-bucket-securesight"
+
+  tags = {
+    Name        = "Terraform State Bucket"
+    Project     = "SecureSight"
+    ManagedBy   = "Terraform Bootstrap"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_encryption" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "terraform_state_block" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name      = "Terraform State Lock"
+    Project   = "SecureSight"
+    ManagedBy = "Terraform Bootstrap"
+  }
+}
